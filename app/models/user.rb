b38c31f7bf406b9198a5ad88d2d0e3fadce3e317
mariela <gila44dri@gmail.com>
2020-03-18 23:01:39 -0300

    mejora de detalles y card deck de productos

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :products, through: :orders
-  has_many :orders    
+  has_many :orders  
+  
+  
+  
 end
