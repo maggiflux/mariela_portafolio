981a952701b2e83194f1124f45478f83ec92f848
mariela <gila44dri@gmail.com>
2020-03-19 13:22:14 -0300

    destroy arreglado

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,7 @@
 class Product < ApplicationRecord
   belongs_to :user, optional: true
   has_many :category
-  has_many :orders
+  has_many :orders, dependent: :destroy
   has_one_attached :image
 
 end
